559e641813d1e69b745b487893dafb1f6cef45f2
Thalisson da Rosa <thalisson.rosa@arctouch.com>
2020-02-15 15:36:01 -0300

    Creating project and adding Cocoapods
    After some consideration and to make it easier to run the project, all the pods are going to be commited to repository.

---
+++ Timeline/Timeline/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Timeline
+//
+//  Created by Thalisson da Rosa on 15/02/20.
+//  Copyright © 2020 Thalisson da Rosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
